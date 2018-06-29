ede40f542aa845e34fe4bd7fe9e149b67e72744f
Emperor <daniel.cs20@gmail.com>
2018-06-29 14:06:29 +0200

    GET /todos/:id route

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import {json} from "body-parser";
 import {connect} from "mongoose";
+import {ObjectId} from 'mongodb';
 
 import {Todo} from "./models/todo.model";
 
@@ -17,6 +18,23 @@ app.get('/todos', (req, res) => {
    Todo.find().then(todos => {
        res.send({todos});
    }).catch(err => res.status(400).send(err));
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        res.status(400).send({error: 'Invalid id!'});
+        return;
+    }
+    Todo.findById(req.params.id)
+        .then(todo => {
+            if (todo) {
+                res.send({todo});
+                return;
+            }
+            res.status(404).send({error: 'Could not find todo with that id!'});
+        })
+        .catch(err => res.status(400).send({error: 'Could not reach database!'}))
 });
 
 app.post('/todos', (req, res) => {

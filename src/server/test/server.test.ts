7c54ec3e962c45e2eb8b004696c4ec07c17139ff
Emperor <daniel.cs20@gmail.com>
2018-06-29 12:33:40 +0200

    GET /todos test

--- src/server/test/server.test.ts
+++ src/server/test/server.test.ts
@@ -5,9 +5,19 @@ import {it, describe, beforeEach} from "mocha";
 import {Todo} from "../models/todo.model";
 import {app} from "../server";
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done: any) => {
     Todo.remove({})
-        .then(() => done())
+        .then(() => {
+            return Todo.insertMany(todos, (err, doc) => {
+                if (err) return done(err);
+            });
+        }).then(() => done())
         .catch(err => done(err));
 });
 
@@ -25,7 +35,7 @@ describe('POST /todos', () => {
               if (err) {
                   return done(err);
               }
-              Todo.find().then(todos => {
+              Todo.find({text}).then(todos => {
                   expect(todos.length).toBe(1);
                   expect(todos[0].text).toBe(text);
                   done();
@@ -43,9 +53,24 @@ describe('POST /todos', () => {
                   return done(err);
               }
               Todo.find().then(todos => {
-                  expect(todos.length).toBe(0);
+                  expect(todos.length).toBe(2);
                   done();
               }).catch(e => done(e));
           });
    });
+});
+
+describe('GET /todos', () => {
+
+    it('should fetch all the todos from the server', done => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res: any) => {
+                expect(res.body.todos.length)
+                    .toBe(2);
+            })
+            .end(done);
+    });
+
 });
